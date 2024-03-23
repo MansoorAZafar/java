e00cef83492c5f980d84d827f8898469a621eb2b
mansoor zafar <164381371+MansoorAZafar@users.noreply.github.com>
2024-03-23 12:58:26 -0400

    Delete Part 3 directory

--- Part 3/Room/Room.java
+++
@@ -1,9 +0,0 @@
-public class Room {
-    private String code;
-    private int    seats;
-
-    public Room(String code, int nSeats) {
-        this.code = code;
-        this.seats = nSeats;
-    }
-}
